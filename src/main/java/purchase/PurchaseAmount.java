82289441f0348a9d3bdece0746011ec8ab70aa70
BaumAss <105116744+BaumAss@users.noreply.github.com>
2023-12-11 01:56:51 +0300

    Исправил замечания по комментариям, инкапсуляции, использовал BigDecimal, создал отдельный класс для подсчета цены и вместо класса Arraylist использовал интерфейс Arrays

---
+++ src/main/java/purchase/PurchaseAmount.java
@@ -0,0 +1,45 @@
+package purchase;
+
+import java.math.BigDecimal;
+
+/*
+ * Класс для подсчета суммы покупок с учетом скидок
+ * В классе есть три поля: кол-во вещей, цена и скидка
+ * В классе реализованы сеттеры
+ */
+public class PurchaseAmount {
+    private int countThings; //Кол-во вещей
+    private BigDecimal price; // Цена одного товара
+    private BigDecimal discount; // Скидка
+
+    // Конструктор с параметрами
+    public PurchaseAmount(int countThings, float price, float discount) throws Exception{
+        if (countThings >= 0 && price >= 0.0F && (discount >= 0.0F && discount <= 100.0F)){
+            this.countThings = countThings;
+            this.price = new BigDecimal(price);
+            this.discount = new BigDecimal(discount);
+    }
+        else{
+            throw new Exception("Проверьте правильность ввода данных!");
+        }
+    }
+    public void setCountThings(int countThings) {
+        this.countThings = countThings;
+    }
+    public void setPrice(float price){
+        this.price = new BigDecimal(price);
+    }
+    public void setDiscount(float discount) {
+        this.discount = new BigDecimal(discount);
+    }
+
+    public BigDecimal getPrice(){
+        return this.price;
+    }
+    public BigDecimal getDiscount(){
+        return this.discount;
+    }
+    public int getCountThings(){
+        return this.countThings;
+    }
+}
